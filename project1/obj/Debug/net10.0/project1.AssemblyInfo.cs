d210e9271403d2a2c83a6987a24c76c0cd510d8e
JacquelineRepo <jakubkarczewski3409@gmail.com>
2026-03-18 09:00:20 +0100

    Added more to task description and updated rider packages

--- project1/obj/Debug/net10.0/project1.AssemblyInfo.cs
+++ project1/obj/Debug/net10.0/project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8cc862579695841c5f046ad4daa1cab6d7b93d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
