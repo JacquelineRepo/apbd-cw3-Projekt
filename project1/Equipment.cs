790f64a1ea2ebd3935024791c00a8ed88758fd2b
JacquelineRepo <jakubkarczewski3409@gmail.com>
2026-03-18 09:39:16 +0100

    Updated Equipment with properties and methods

--- project1/Equipment.cs
+++ project1/Equipment.cs
@@ -2,5 +2,32 @@
 
 public class Equipment
 {
+    public static int Id{
+        get;
+        set;
+    }
+    public static string Name{
+        get;
+        set;
+    }
+    public bool Availability{
+        get;
+        set;
+    }
+    
+    public Equipment(string name)
+    {
+        Id++;
+        Name = name;
+        Availability = true;
+        
+    }
+    public void changeStatus()
+    {
+        Availability = !Availability;
+    }
+    
+    
+    
     
 }
